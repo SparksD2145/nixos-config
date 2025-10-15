40389c881d11f6ede57731b5725f45a468683ee3
Thomas Ibarra <hello@iwrite.software>
2025-10-15 13:19:24 -0500

    bugfix(delta): correction to gnome paths

--- config/hosts/delta/default.nix
+++ config/hosts/delta/default.nix
@@ -21,6 +21,7 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "alpha"; # Define your hostname.
+  networking.networkmanager.enable = true; # Enable NetworkManager
   networking.wireless.enable = true; # Enables wireless support via wpa_supplicant.
   networking.wireless.secretsFile = "/run/secrets/networking/wireless.conf";
   networking.wireless.networks = {
