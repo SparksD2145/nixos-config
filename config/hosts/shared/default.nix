94e264ab8dde97206baa049be229effd76661671
Thomas Ibarra <hello@iwrite.software>
2025-10-15 14:32:31 -0500

    feat(systems): disable manual user management

--- config/hosts/shared/default.nix
+++ config/hosts/shared/default.nix
@@ -31,4 +31,7 @@
 
   # Fuse filesystem that returns symlinks to executables based on the PATH of the requesting process. This is useful to execute shebangs on NixOS that assume hard coded locations in locations like /bin or /usr/bin etc.
   services.envfs.enable = true;
+
+  # Enable user management by nixos-rebuild. Disable manual user management.
+  users.mutableUsers = false;
 }
