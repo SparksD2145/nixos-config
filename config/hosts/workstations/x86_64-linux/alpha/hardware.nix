27625e3d2c14b36e2c1a382de276374daef237f7
Thomas Ibarra <hello@iwrite.software>
2025-11-08 15:22:37 -0600

    alpha: add bluetooth

--- config/hosts/workstations/x86_64-linux/alpha/hardware.nix
+++ config/hosts/workstations/x86_64-linux/alpha/hardware.nix
@@ -65,4 +65,7 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  # enable bluetooth
+  hardware.bluetooth.enable = true;
 }
