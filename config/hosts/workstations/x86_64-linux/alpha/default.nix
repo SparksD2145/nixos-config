4fe3cb644fafa52dce7d2f15605d533a02ed7dfc
Thomas Ibarra <hello@iwrite.software>
2025-12-15 11:22:36 -0600

    programs: add gamescope

--- config/hosts/workstations/x86_64-linux/alpha/default.nix
+++ config/hosts/workstations/x86_64-linux/alpha/default.nix
@@ -65,6 +65,8 @@
 
   # Let there be games
   programs.steam.enable = true;
+  programs.steam.protontricks.enable = true;
+  programs.gamescope.enable = true;
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
