a12ee242d96b4c5415a37be68b08cf22dfd55e57
Thomas Ibarra <hello@iwrite.software>
2025-12-16 12:40:27 -0600

    services: configure settings for openssh

--- config/hosts/_global/default.nix
+++ config/hosts/_global/default.nix
@@ -26,7 +26,13 @@
   programs.zsh.enable = true;
 
   # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
+  services.openssh = {
+    enable = true;
+    settings = {
+      PermitRootLogin = "no";
+      PasswordAuthentication = true;
+    };
+  };
 
   # Fuse filesystem that returns symlinks to executables based on the PATH of the requesting process. This is useful to execute shebangs on NixOS that assume hard coded locations in locations like /bin or /usr/bin etc.
   services.envfs.enable = true;
