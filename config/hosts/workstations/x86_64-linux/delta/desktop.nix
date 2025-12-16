8f5eb09f00c1387c7460f968dc89a9d49e981be1
Thomas Ibarra <hello@iwrite.software>
2025-12-16 12:46:51 -0600

    delta: update gnome nix settings

--- config/hosts/workstations/x86_64-linux/delta/desktop.nix
+++ config/hosts/workstations/x86_64-linux/delta/desktop.nix
@@ -4,8 +4,8 @@
   services.xserver.enable = true;
 
   # Enable the KDE Plasma Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
