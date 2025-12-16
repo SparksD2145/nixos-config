f3c26f54fab47cb364f4f451943f707d7c9cd670
Thomas Ibarra <hello@iwrite.software>
2025-12-16 14:18:53 -0600

    home-manager: force vrpath to be created

--- config/hosts/workstations/x86_64-linux/alpha/games.nix
+++ config/hosts/workstations/x86_64-linux/alpha/games.nix
@@ -21,25 +21,30 @@
   systemd.user.services.monado.environment = {
     STEAMVR_LH_ENABLE = "1";
     XRT_COMPOSITOR_COMPUTE = "1";
+    XRT_COMPOSITOR_USE_PRESENT_WAIT = "1";
+    U_PACING_COMP_TIME_FRACTION_PERCENT = "90";
   };
   home-manager.users.sparks.home.file.".config/openxr/1/active_runtime.json".source =
     "${pkgs.monado}/share/openxr/1/openxr_monado.json";
 
-  home-manager.users.sparks.home.file.".config/openvr/openvrpaths.vrpath".text =
-    let
-      steam = "/home/sparks/.local/share/Steam";
-    in
-    builtins.toJSON {
-      version = 1;
-      jsonid = "vrpathreg";
+  home-manager.users.sparks.home.file.".config/openvr/openvrpaths.vrpath" = {
+    force = true;
+    text =
+      let
+        steam = "/home/sparks/.local/share/Steam";
+      in
+      builtins.toJSON {
+        version = 1;
+        jsonid = "vrpathreg";
 
-      external_drivers = null;
-      config = [ "${steam}/config" ];
+        external_drivers = null;
+        config = [ "${steam}/config" ];
 
-      log = [ "${steam}/logs" ];
+        log = [ "${steam}/logs" ];
 
-      "runtime" = [
-        "${pkgs.xrizer}/lib/xrizer"
-      ];
-    };
+        "runtime" = [
+          "${pkgs.xrizer}/lib/xrizer"
+        ];
+      };
+  };
 }
