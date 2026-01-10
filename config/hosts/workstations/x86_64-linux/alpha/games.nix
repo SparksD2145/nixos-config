4f9b834474217e0f91bea841ae11b638d088380c
Thomas Ibarra <hello@iwrite.software>
2026-01-10 16:15:53 -0600

    alpha: remove steam vr settings

--- config/hosts/workstations/x86_64-linux/alpha/games.nix
+++ config/hosts/workstations/x86_64-linux/alpha/games.nix
@@ -3,14 +3,6 @@
   # Let there be games
   programs.steam = {
     enable = true;
-    package = pkgs.steam.override {
-      extraProfile = ''
-        # Fixes timezones on VRChat
-        unset TZ
-        # Allows Monado to be used
-        export PRESSURE_VESSEL_IMPORT_OPENXR_1_RUNTIMES=1
-      '';
-    };
   };
 
   # Minecraft Launcher
