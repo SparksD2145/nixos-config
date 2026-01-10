ba0464025e08dc5bf234d19e879d3b84f11b6cb3
Thomas Ibarra <hello@iwrite.software>
2026-01-10 16:13:15 -0600

    alpha: remove vr capabilities

--- config/hosts/workstations/x86_64-linux/alpha/games.nix
+++ config/hosts/workstations/x86_64-linux/alpha/games.nix
@@ -13,42 +13,6 @@
     };
   };
 
-  # VR Configuration
-  services.monado = {
-    enable = true;
-    defaultRuntime = true; # Register as default OpenXR runtime
-    highPriority = true;
-  };
-  systemd.user.services.monado.environment = {
-    STEAMVR_LH_ENABLE = "1";
-    XRT_COMPOSITOR_COMPUTE = "1";
-    XRT_COMPOSITOR_USE_PRESENT_WAIT = "1";
-    U_PACING_COMP_TIME_FRACTION_PERCENT = "90";
-  };
-  home-manager.users.sparks.home.file.".config/openxr/1/active_runtime.json".source =
-    "${pkgs.monado}/share/openxr/1/openxr_monado.json";
-
-  home-manager.users.sparks.home.file.".config/openvr/openvrpaths.vrpath" = {
-    force = true;
-    text =
-      let
-        steam = "/home/sparks/.local/share/Steam";
-      in
-      builtins.toJSON {
-        version = 1;
-        jsonid = "vrpathreg";
-
-        external_drivers = null;
-        config = [ "${steam}/config" ];
-
-        log = [ "${steam}/logs" ];
-
-        "runtime" = [
-          "${pkgs.xrizer}/lib/xrizer"
-        ];
-      };
-  };
-
   # Minecraft Launcher
   environment.systemPackages = with pkgs; [
     prismlauncher
