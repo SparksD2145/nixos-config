d7c11e674996d7365b38a4e956ea7727c2d26246
Thomas Ibarra <hello@iwrite.software>
2026-02-11 11:09:00 -0600

    nix: enable automatic optimization and gc

--- config/hosts/_global/default.nix
+++ config/hosts/_global/default.nix
@@ -7,6 +7,20 @@
   # Set your time zone.
   time.timeZone = "America/Chicago";
 
+  # Enable Automatic optimization of nix store.
+  nix.optimise.automatic = true;
+  nix.optimise.dates = [ "14:00" ];
+
+  # Enable Automatic optimization of nix store at build.
+  nix.settings.auto-optimise-store = true;
+
+  # Enable Automatic garbage collection of nix store.
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 15d";
+  };
+
   # Enable nix-command and flakes
   nix.settings.experimental-features = [
     "nix-command"
