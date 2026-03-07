d8cab8f954c0632b6fdd5f214aa44ad147664bcb
Thomas Ibarra <hello@iwrite.software>
2026-03-07 11:47:24 -0600

    ssh(sparks): add authorized pub keys

--- config/users/sparks/system/default.nix
+++ config/users/sparks/system/default.nix
@@ -27,6 +27,13 @@
     ];
     shell = pkgs.zsh;
 
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINAZvl1OGc2mmdcI9tTAwwdmDaV+aKeJ0mDJB3sdwnbk"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICGu/I2uiUKP5qRB7+IcXapKMyOHEJ/CE2/WPpwcmUu9"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJytfM3FKkQSR8j0TvNH4KSfXu84CotTu4o2igJbF3eo"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGeQrWCotrsgqZ8sqeRcMzVL5dov97tq5Tu0qVvJSj02"
+    ];
+
     hashedPasswordFile = config.sops.secrets."users/sparks/passwd".path;
   };
 }
