5476a8cc455988687a00d8ba596e160fe5e2ebc5
Thomas Ibarra <hello@iwrite.software>
2026-03-08 12:37:56 -0500

    k3s: disable firewall

--- config/hosts/servers/_shared/k3s/k3s-agent.nix
+++ config/hosts/servers/_shared/k3s/k3s-agent.nix
@@ -9,6 +9,8 @@
     tokenFile = config.sops.secrets."k3s/node-token".path;
   };
 
+  networking.firewall.enable = false;
+
   systemd.services.containerd.serviceConfig = {
     LimitNOFILE = lib.mkForce null;
   };
