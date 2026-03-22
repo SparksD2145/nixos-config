4760a215946a40987c1bf92278aa4003126bb2ac
Thomas Ibarra <hello@iwrite.software>
2026-03-22 11:17:57 -0500

    add code alias

--- config/users/sparks/home-manager/default.nix
+++ config/users/sparks/home-manager/default.nix
@@ -55,6 +55,7 @@
       # set some aliases, feel free to add more or remove some
       shellAliases = {
         k = "kubectl";
+        code = "codium";
         urldecode = "python3 -c 'import sys, urllib.parse as ul; print(ul.unquote_plus(sys.stdin.read()))'";
         urlencode = "python3 -c 'import sys, urllib.parse as ul; print(ul.quote_plus(sys.stdin.read()))'";
       };
