0f244225c0577062ca5e3d09eef818225b98ae42
Thomas Ibarra <hello@iwrite.software>
2026-03-23 15:24:03 -0500

    add intellicode 2

--- config/users/sparks/default.nix
+++ config/users/sparks/default.nix
@@ -168,7 +168,7 @@
         ms-python.python
         ms-azuretools.vscode-docker
         ms-vscode-remote.remote-ssh
-        VisualStudioExptTeam.vscodeintellicode
+        visualstudioexptteam.vscodeintellicode
         eamodio.gitlens
       ];
     };
