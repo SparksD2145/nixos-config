6ddd4e556e4dbd82cbb8b9f7ed3511419ee2c90a
Thomas Ibarra <hello@iwrite.software>
2026-04-05 14:02:06 -0500

    beta: add br0 bridge network

---
+++ config/hosts/servers/beta/network.nix
@@ -0,0 +1,13 @@
+{ ... }:
+{
+  # Enable a bridge network for VM access.
+  networking.interfaces."enp7s0".useDHCP = false;
+  networking.interfaces."br0".useDHCP = true;
+  networking.bridges = {
+    br0 = {
+      interfaces = [
+        "enp7s0"
+      ];
+    };
+  }
+}
