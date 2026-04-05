052b5934f3cd9798415d0ace1299501502dea6b6
Thomas Ibarra <hello@iwrite.software>
2026-04-05 16:03:11 -0500

    beta(network): disable networkmanager 3

--- config/hosts/servers/beta/network.nix
+++ config/hosts/servers/beta/network.nix
@@ -1,20 +1,19 @@
 { ... }:
 {
   # Enable a bridge network for VM access.
-  networking.networkmanager.enable = false;
-  networking.useDHCP = false;
-  networking.interfaces."enp7s0".useDHCP = true;
-  networking.interfaces."br0".useDHCP = true;
-  networking.bridges = {
-    br0 = {
-      interfaces = [
-        "enp7s0"
-      ];
+  networking = {
+    networkmanager.enable = false;
+    useNetworkd = true;
+    bridges.br0.interfaces = [ "enp7s0" ];
+    interfaces.br0 = {
+      useDHCP = true;
     };
+
+    firewall.trustedInterfaces = [
+      "lo"
+      "br0"
+    ];
   };
 
-  networking.firewall.trustedInterfaces = [
-    "lo"
-    "br0"
-  ];
+  systemd.services.dhcpcd.after = [ "sys-subsystem-net-devices-br0.device" ];
 }
