5c25d85f1aa28fef20b472543047483c2b260adf
Thomas Ibarra <hello@iwrite.software>
2026-04-10 15:17:26 -0500

    ntfy: replace ntfy dotfile with sops config 2

--- config/modules/hosts-users/sparks/dotfiles/ntfy.nix
+++ config/modules/hosts-users/sparks/dotfiles/ntfy.nix
@@ -6,7 +6,6 @@
 {
   sops.secrets = {
     "users/sparks/ntfy_config" = {
-      neededForUsers = true;
       mode = "0440";
       owner = config.users.users.sparks.name;
       group = config.users.users.sparks.group;
