4f5354719b0bcfdf649decb31e2e22285668f8a9
Thomas Ibarra <hello@iwrite.software>
2026-04-11 19:30:56 -0500

    scripts: remove hostname argument

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,9 +1,2 @@
-# Accept hostname as an argument
-if [ -z "$1" ]; then
-    system_hostname = $1
-else
-    exit 1;
-fi
-
 # INSTALL
-sudo nixos-install --root /mnt --flake "github:SparksD2145/nixos-config#${system_hostname}"
+sudo nixos-install --root /mnt --flake "github:SparksD2145/nixos-config#system_hostname"
