588bee14b454e7a2411ce15ed62064953b1bab2b
Thomas Ibarra <hello@iwrite.software>
2026-04-23 12:47:53 -0500

    delta(noctalia): ignore settings temp

--- config/modules/noctalia/default.nix
+++ config/modules/noctalia/default.nix
@@ -6,7 +6,7 @@
       packages.myNoctalia = inputs.wrapper-modules.wrappers.noctalia-shell.wrap {
         inherit pkgs;
 
-        settings = (builtins.fromJSON (builtins.readFile ./noctalia.json)).settings;
+        # settings = (builtins.fromJSON (builtins.readFile ./noctalia.json)).settings;
       };
     };
 }
