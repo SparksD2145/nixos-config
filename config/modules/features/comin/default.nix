60cd394ca2792f3547fcf3ddb257d7435d613a86
Thomas Ibarra <hello@iwrite.software>
2026-05-07 08:48:36 -0500

    chore: move to sparks/homelab repository

--- config/modules/features/comin/default.nix
+++ config/modules/features/comin/default.nix
@@ -12,14 +12,14 @@
         remotes = [
           {
             name = "origin";
-            url = "https://github.com/SparksD2145/nixos-config.git";
+            url = "https://github.com/SparksD2145/homelab.git";
             branches.main.name = "master";
             poller.period = 900; # Poll every 15 minutes
             auth.access_token_path = "/run/secrets/comin/gh_token";
           }
           {
             name = "gitlab";
-            url = "https://gitlab.iwrite.software/sparks/nixos-config.git";
+            url = "https://gitlab.iwrite.software/sparks/homelab.git";
             branches.main.name = "master";
             poller.period = 30; # Poll every 30 seconds
             auth.access_token_path = "/run/secrets/comin/glab_token";
